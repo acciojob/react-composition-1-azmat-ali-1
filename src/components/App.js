52fcadb47e3a7b367973cb17acf1ded2c347ef5b
AZMAT ALI <222azmat@gmail.com>
2023-08-05 20:00:42 +0530

    submitting code-Sat Aug 05 2023 20:00:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,34 @@ import React from "react";
 import './../styles/App.css';
 import { useState } from 'react';
 import tabs from './Tabs';
-const App = () => {
-   const [activeTab, setActiveTab] = useState(0);
 
-  const handleTabClick = (index) => {
-    setActiveTab(index);
-  };
+
+const App = () => {
+  const tabs = [
+    {
+      title: 'Tab 1',
+      content: 'Content for Tab 1',
+    },
+    {
+      title: 'Tab 2',
+      content: 'Content for Tab 2',
+    },
+    {
+      title: 'Tab 3',
+      content: 'Content for Tab 3',
+    },
+  ];
+
+  const [activeTab, setActiveTab] = useState(0);
+  
   return (
     <div>
-        {/* Do not remove the main div */}
-         <div>
+      <h1>Tabs Example</h1>
       <ul>
         {tabs.map((tab, index) => (
           <li
             key={index}
-            onClick={() => handleTabClick(index)}
+            onClick={() => setActiveTab(index)}
             style={{ cursor: 'pointer', fontWeight: index === activeTab ? 'bold' : 'normal' }}
           >
             {tab.title}
@@ -25,7 +38,6 @@ const App = () => {
         ))}
       </ul>
       <div>{tabs[activeTab].content}</div>
-    </div>
     </div>
   )
 }
