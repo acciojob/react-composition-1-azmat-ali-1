98049e90cf5dff6871c540c1be8e8cad2980328a
AZMAT ALI <222azmat@gmail.com>
2023-08-05 19:43:25 +0530

    submitting code-Sat Aug 05 2023 19:43:24 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,56 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useState } from 'react';
 const App = () => {
+   const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
   return (
     <div>
         {/* Do not remove the main div */}
+         <div>
+      <ul>
+        {tabs.map((tab, index) => (
+          <li
+            key={index}
+            onClick={() => handleTabClick(index)}
+            style={{ cursor: 'pointer', fontWeight: index === activeTab ? 'bold' : 'normal' }}
+          >
+            {tab.title}
+          </li>
+        ))}
+      </ul>
+      <div>{tabs[activeTab].content}</div>
+    </div>
     </div>
   )
 }
+
+const TabsApp = () => {
+  const tabs = [
+    {
+      title: 'Tab 1',
+      content: 'Content for Tab 1',
+    },
+    {
+      title: 'Tab 2',
+      content: 'Content for Tab 2',
+    },
+    {
+      title: 'Tab 3',
+      content: 'Content for Tab 3',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Tabs Example</h1>
+      <Tabs tabs={tabs} />
+    </div>
+  );
+};
 
 export default App
